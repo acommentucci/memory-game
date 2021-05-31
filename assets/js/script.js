2256565aa1c3ccc3d02f628066b90a100d090ca9
acommentucci <a.commentucci@gmail.com>
2021-05-31 19:05:26 +0100

    fix: cardArray function moved on top

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,3 @@
-// Get the modal
-let howToModal = document.getElementById("how-to-modal");
-let startModal = document.getElementById("start-modal");
-
-// Get the button that opens the modal
-let howToBtn = document.getElementById("button-how-to");
-let startBtn = document.getElementById("button-start");
-
-// Get the <span> element that closes the modal
-let quitHowTo = document.getElementsByClassName("closeHowTo")[0];
-let quitStart = document.getElementsByClassName("closeStart")[0];
-
-// When the user clicks the button, open the modal 
-howToBtn.onclick = function() {
-  howToModal.style.display = "block";
-}
-startBtn.onclick = function() {
-    startModal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-quitHowTo.onclick = function() {
-    howToModal.style.display = "none";
-}
-quitStart.onclick = function() {
-    startModal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == howToModal) {
-    howToModal.style.display = "none";
-  }
-  if (event.target == startModal) {
-      startModal.style.display = "none";
-    }
-  if (event.target == gameModal) {
-        gameModal.style.display = "none";
-      }
-}
-
-
-//game 
-let gameModal = document.getElementById("game-modal");
-
-//choose level
-let easy = document.getElementById("easy");
-let medium = document.getElementById("medium");
-let hard = document.getElementById("hard");
-easy.onclick = easyGame()
-medium.onclick = "mediumGame()"
-hard.onclick = "hardGame()"
-
-function easyGame(){
-    gameModal.style.display = "block";
-    startModal.style.display = "none";
-    easyCardArray = cardArray.slice(0, 9)
-    createBoard(easyCardArray)
-}
-
-
-// When the user clicks the button, open the modal 
-easy.onclick = function() {
-    gameModal.style.display = "block";
-    startModal.style.display = "none";
-  }
-
 //cards
 let cardArray = [
     {
@@ -148,6 +81,73 @@ let cardArray = [
         img: './assets/img/Cards/Linconer.jpg',
     },
 ]
+// Get the modal
+let howToModal = document.getElementById("how-to-modal");
+let startModal = document.getElementById("start-modal");
+
+// Get the button that opens the modal
+let howToBtn = document.getElementById("button-how-to");
+let startBtn = document.getElementById("button-start");
+
+// Get the <span> element that closes the modal
+let quitHowTo = document.getElementsByClassName("closeHowTo")[0];
+let quitStart = document.getElementsByClassName("closeStart")[0];
+
+// When the user clicks the button, open the modal 
+howToBtn.onclick = function() {
+  howToModal.style.display = "block";
+}
+startBtn.onclick = function() {
+    startModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+quitHowTo.onclick = function() {
+    howToModal.style.display = "none";
+}
+quitStart.onclick = function() {
+    startModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == howToModal) {
+    howToModal.style.display = "none";
+  }
+  if (event.target == startModal) {
+      startModal.style.display = "none";
+    }
+  if (event.target == gameModal) {
+        gameModal.style.display = "none";
+      }
+}
+
+
+//game 
+let gameModal = document.getElementById("game-modal");
+
+//choose level
+let easy = document.getElementById("easy");
+let medium = document.getElementById("medium");
+let hard = document.getElementById("hard");
+easy.onclick = easyGame()
+medium.onclick = "mediumGame()"
+hard.onclick = "hardGame()"
+
+function easyGame(){
+    gameModal.style.display = "block";
+    startModal.style.display = "none";
+    easyCardArray = cardArray.slice(0, 9)
+    createBoard(easyCardArray)
+}
+
+
+// When the user clicks the button, open the modal 
+easy.onclick = function() {
+    gameModal.style.display = "block";
+    startModal.style.display = "none";
+  }
+
 
 cardArray.sort(() => 0.5 - Math.random())
 
